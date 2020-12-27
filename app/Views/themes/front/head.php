361acc7377a4662480bd09b1f2d5b7b7d60a9044
permanaloremIpsum <dpermana350@gmail.com>
2020-12-27 22:59:19 +0700

    detail page

--- app/Views/themes/front/head.php
+++ app/Views/themes/front/head.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <link href="<?= base_url() ?>/themes/assets/css/owl.carousel.min.css" rel="stylesheet">
     <link href="<?= base_url() ?>/themes/assets/css/owl.theme.default.css" rel="stylesheet">
+    <link href="<?= base_url() ?>/themes/assets/css/lightslider.css" rel="stylesheet">
     <link href="<?= base_url() ?>/themes/assets/css/main.css" rel="stylesheet">
 
     <script src="<?= base_url() ?>/themes/assets/js/jquery.min.js" rel="stylesheet"></script>
@@ -18,14 +19,15 @@
     <script src="<?= base_url() ?>/themes/assets/js/jquery.easing.min.js" rel="stylesheet"></script>
     <script src="<?= base_url() ?>/themes/assets/js/owl.carousel.js" rel="stylesheet"></script>
     <script src="<?= base_url() ?>/themes/assets/js/owl.carousel.min.js" rel="stylesheet"></script>
+    <script src="<?= base_url() ?>/themes/assets/js/lightslider.js" rel="stylesheet"></script>
     <script src="<?= base_url() ?>/themes/assets/js/style.js" rel="stylesheet"></script>
     <link rel="stylesheet" href="<?= base_url() ?>/themes/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
     <script src="<?= base_url() ?>/themes/plugins/sweetalert2/sweetalert2.min.js"></script>
-    
 
-    <?php if($request->uri->getSegment(1) == 'carts'): ?>
+
+    <?php if ($request->uri->getSegment(1) == 'carts') : ?>
         <!-- DataTables -->
-    <link rel="stylesheet" href="<?= base_url() ?>/themes/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+        <link rel="stylesheet" href="<?= base_url() ?>/themes/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
 
     <?php endif; ?>
 
